98c85c9477eeaa730bf0a305458a1d4ccfa9192c
srikanth-nuthalapati <srikanthnuthalapati@outlook.com>
2024-10-22 11:28:53 +0530

    msg

--- RBP/script.js
+++ RBP/script.js
@@ -62,201 +62,6 @@ let currentMonth = dateObj.getMonth() + 1;
 let currentDate = dateObj.getDate();
 
 
-
-let data = [
-    {
-        "id":1,
-        "Train_no":12711,
-        "Train_name":"pinakini express",
-        "source":"vijayawada",
-        "destination":"chennai",
-        "departure_time":"06:10 am",
-        "arrival_time":"01:06 pm",
-        "sehedule":"Runs Daily"
-    },
-    {
-        "id":2,
-        "Train_no":12712,
-        "Train_name":"pinakini express",
-        "source":"chennai",
-        "destination":"vijayawada",
-        "departure_time":"02:5 pm",
-        "arrival_time":"09:35 pm",
-        "sehedule":"Runs Daily"
-    },
-    {
-        "id":3,
-        "Train_no":12709,
-        "Train_name":"simhapuri SF express",
-        "source":"gudur",
-        "destination":"secunderabad",
-        "departure_time":"06:40 pm",
-        "arrival_time":"05:10 am",
-        "sehedule":"Runs Daily"
-    },
-    {
-        "id":4,
-        "Train_no":12710,
-        "Train_name":"simhapuri SF express",
-        "source":"secunderabad",
-        "destination":"gudur",
-        "departure_time":"11:05 pm",
-        "arrival_time":"09:40 am",
-        "sehedule":"Runs Daily"
-    },
-    {
-        "id":5,
-        "Train_no":20707,
-        "Train_name":"visakhapatnam vande bharat express",
-        "source":"secunderabad",
-        "destination":"visakhapatnam",
-        "departure_time":"5:05 am",
-        "arrival_time":"01:50 pm",
-        "sehedule":"S M T W F S"
-    },
-    {
-        "id":6,
-        "Train_no":20708,
-        "Train_name":"secunderabad vande bharat express",
-        "source":"visakhapatnam",
-        "destination":"secunderabad",
-        "departure_time":"02:35 pm",
-        "arrival_time":"11:20 pm",
-        "sehedule":"S M T W F S"
-    },
-    {
-        "id":7,
-        "Train_no":12603,
-        "Train_name":"hyderabad sf express",
-        "source":"hyderabad",
-        "destination":"hyderabad",
-        "departure_time":"04:45 pm",
-        "arrival_time":"05:50 am",
-        "sehedule":"S M T W T F S"
-    },
-    {
-        "id":8,
-        "Train_no":12604,
-        "Train_name":"chennai central sf express",
-        "source":"hyderabad",
-        "destination":"hyderabad",
-        "departure_time":"04:45 pm",
-        "arrival_time":"05:40 am",
-        "sehedule":"S M T W T F S"
-    },
-    {
-        "id":9,
-        "Train_no":12759,
-        "Train_name":"charminar express",
-        "source":"chennai",
-        "destination":"secunderabad",
-        "departure_time":"06:05 pm",
-        "arrival_time":"06:35 am",
-        "sehedule":"S M T W T F S"
-    },
-    {
-        "id":10,
-        "Train_no":12760,
-        "Train_name":"charminar express",
-        "source":"secunderabad",
-        "destination":"chennai",
-        "departure_time":"06:25 pm",
-        "arrival_time":"07:00 am",
-        "sehedule":"S M T W T F S"
-    },
-    {
-        "id":11,
-        "Train_no":17480,
-        "Train_name":"tirupati - puri express",
-        "source":"tirupati",
-        "destination":"puri",
-        "departure_time":"10:40 am",
-        "arrival_time":"02:50 pm",
-        "sehedule":"M T W F S"
-    },
-    {
-        "id":12,
-        "Train_no":17479,
-        "Train_name":"puri - tirupati express",
-        "source":"puri",
-        "destination":"tirupati",
-        "departure_time":"06:30 pm",
-        "arrival_time":"10:45 pm",
-        "sehedule":"S M W T F"
-    },
-    {
-        "id":13,
-        "Train_no":12766,
-        "Train_name":"tirupati sf express",
-        "source":"amravati",
-        "destination":"tirupati",
-        "departure_time":"06:45 am",
-        "arrival_time":"06:25 am",
-        "sehedule":"M T"
-    },
-    {
-        "id":14,
-        "Train_no":12765,
-        "Train_name":"amravati sf express",
-        "source":"tirupati",
-        "destination":"amravati",
-        "departure_time":"03:35 pm",
-        "arrival_time":"03:10 pm",
-        "sehedule":"T S"
-    },
-    {
-        "id":15,
-        "Train_no":18048,
-        "Train_name":"kacheguda vsg shm exp",
-        "source":"goa",
-        "destination":"kacheguda",
-        "departure_time":"06:30 am",
-        "arrival_time":"05:00 am",
-        "sehedule":"S T T F"
-    },
-    {
-        "id":16,
-        "Train_no":18189,
-        "Train_name":"ernakulam jn. exp",
-        "source":"tatanagar",
-        "destination":"ernakulam",
-        "departure_time":"05:15 am",
-        "arrival_time":"01:55 am",
-        "sehedule":"S M T W T F S"
-    },
-    {
-        "id":17,
-        "Train_no":18190,
-        "Train_name":"tatanagar exp",
-        "source":"ernakulam",
-        "destination":"tatanagar",
-        "departure_time":"07:15 am",
-        "arrival_time":"05:00 am",
-        "sehedule":"S M T W T F S"
-    },
-    {
-        "id":18,
-        "Train_no":17229,
-        "Train_name":"sabari exp",
-        "source":"thiruvananthapuram",
-        "destination":"secunderabad",
-        "departure_time":"06:15 am",
-        "arrival_time":"12:30 pm",
-        "sehedule":"S M T W T F S"
-    },
-    {
-        "id":19,
-        "Train_no":17230,
-        "Train_name":"sabari exp",
-        "source":"secunderabad",
-        "destination":"thiruvananthapuram",
-        "departure_time":"12:20 pm",
-        "arrival_time":"06:05 pm",
-        "sehedule":"S M T W T F S"
-    },
-]
-
-    
    
 loginPage.classList.add("active");
 
@@ -295,6 +100,7 @@ function generatePNR(){
 //book button click event
 bookingbtn.addEventListener("click",(e)=>{
     e.preventDefault();
+    
     let arr = SelectedDate.value.split("-");
     let date = arr[2];
     let month = arr[1];
@@ -303,58 +109,58 @@ bookingbtn.addEventListener("click",(e)=>{
     if(log_in){
         if(date!=null && boardingStation.value!='' && destinationStation.value!=''){
             if(date>=currentDate && month>=currentMonth && year>=currentYear){
-                let TrainAvailable = false;
-                for(let i=0;i<data.length;i++){
-                    if(data[i].source == boardingStation.value.toLowerCase() && data[i].destination == destinationStation.value.toLowerCase()){
-                        TrainAvailable = true;
-                        let PnrG = generatePNR(); //stored generated pnr in pnrG
-                        BookedTicketTrain.textContent = `Train Name : ${data[i].Train_name.toUpperCase()}`;
-                        BookedTrainNo.textContent = `TRAIN NO : ${data[i].Train_no}`;
-                        BookedTicketPnr.textContent = `PNR NO : ${PnrG}`;
-                        BookedTicketSrc.textContent = `From : ${data[i].source}`;
-                        BookedTicketDes.textContent = `To : ${data[i].destination}`;
-                        Berthdetails.textContent = `Seat : ${generatedTicketNo}/${generatedBerth}`;
-                        BookedTicketClass.textContent = `Class : ${SelectedClass.value}`;
-                        BookedTicketDate.textContent = `Date : ${SelectedDate.value}`;
-                        ticketContainer.style.display = "block";
-                        bookingForm.classList.add("media");
+                    
+                    fetch("http://localhost:3000/trains")
+                    .then(response => response.json())
+                    .then(data => {
+                            const train = data.find(train => train.source === boardingStation.value.toLowerCase() && train.destination === destinationStation.value.toLowerCase());
+                            if(train){                   
+                                let PnrG = generatePNR(); 
+                                BookedTicketTrain.textContent = `Train Name : ${train.Train_name.toUpperCase()}`;
+                                BookedTrainNo.textContent = `TRAIN NO : ${train.Train_no}`;
+                                BookedTicketPnr.textContent = `PNR NO : ${PnrG}`;
+                                BookedTicketSrc.textContent = `From : ${train.source}`;
+                                BookedTicketDes.textContent = `To : ${train.destination}`;
+                                Berthdetails.textContent = `Seat : ${generatedTicketNo}/${generatedBerth}`;
+                                BookedTicketClass.textContent = `Class : ${SelectedClass.value}`;
+                                BookedTicketDate.textContent = `Date : ${SelectedDate.value}`;
+                                ticketContainer.style.display = "block";
+                                bookingForm.classList.add("media");
 
-
-                        let obj = {
-                            "train_no": data[i].Train_no,
-                            "train_name": data[i].Train_name,
-                            "class": SelectedClass.value,
-                            "source": data[i].source,
-                            "destination": data[i].destination,
-                            "timings": data[i].departure_time + " : " + data[i].arrival_time,
-                            "seatNo" : generatedTicketNo + "/" + generatedBerth,
-                            "pnr": PnrG,
-                            "date": date + "-" + month + "-" + year,
-                        }
-                        storeTicketDetail(obj);
-                        break;
-                    }
-                }
-
-                if(TrainAvailable==false){
-                    alert("No Trains Available for this route");
-                }
+                                let obj = {
+                                    "train_no": train.Train_no,
+                                    "train_name": train.Train_name,
+                                    "class": SelectedClass.value,
+                                    "source": train.source,
+                                    "destination": train.destination,
+                                    "timings": train.departure_time + " : " + train.arrival_time,
+                                    "seatNo" : generatedTicketNo + "/" + generatedBerth,
+                                    "pnr": PnrG,
+                                    "date": date + "-" + month + "-" + year,
+                                }
+                                storeTicketDetail(obj);
+                            } else {
+                                alert("No Trains Available for this route");
+                            }
+                    })
+                    .catch(e => console.log(e))
+                                 
             }
             else{
                 alert("Date should not be in past");
             }
         }
+
         else{
           alert("Please enter all fields");
         }
-    }
+    } 
+
     else{
     alert("Please login first");  
     }
     
 });
-
-
 
 pnrStatus.addEventListener("click",()=>{
     pnrContainer.classList.add("active");
@@ -477,7 +283,6 @@ register.addEventListener("click",()=>{
     bookContainer.classList.remove("active");
     TrainSearchPage.classList.remove("active");
 
-    //redirecting to login if already has a account
 });
 
 function storeSignUp(newobj){
